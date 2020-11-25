a4e2a411f7fcd5fe7ad78ecfc73b6fdc93fe09cb
Daniil Levin <levin.dan@ya.ru>
2020-11-25 23:01:27 +0300

    add api template

---
+++ Painteger.Backend/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import werkzeug
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data":"Hello World"}
+
+class LoadImage(Resource):
+    def post(self):
+        parse = reqparse.RequestParser()
+        parse.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
+        args = parse.parse_args()
+        image_file = args['file']
+        image_file.save("flask_test.jpg")
+
+api.add_resource(HelloWorld, "/hello")
+api.add_resource(LoadImage, "/loadImage")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
