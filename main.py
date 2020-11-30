662ce4aca8fc301cd26a4b816224c11b4458367b
Adel Fattakhova <adel.fattakhova.162@gmail.com>
2020-11-30 12:22:00 +0300

    Edit static_url_path

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__
             , static_folder="Painteger.Frontend/build"
-            , static_url_path="/")
+            , static_url_path="")
 api = Api(app)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
